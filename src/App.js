ade06a0760563cceadab1cadd4506493ee46d651
github username <github email address>
2019-10-02 22:36:24 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     this.props.store.dispatch({
       type: 'GET_COUNT_OF_ITEMS',
     });
-  }
+  } 
 
   handleOnClickUsers() {
     this.props.store.dispatch({
@@ -18,7 +18,7 @@ class App extends Component {
   }
 
   render() {
-    // debugger;
+     debugger;
     return (
       <div className="App">
           <button onClick={() => this.handleOnClickItems()}>
